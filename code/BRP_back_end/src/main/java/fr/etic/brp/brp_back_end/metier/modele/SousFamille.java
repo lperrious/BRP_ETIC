4952ba1b2ee5b773e82424c315833d44c7cdd9f3
lperrious <louis30@me.com>
2020-04-24 11:53:00 +0200

    inits

--- code/BRP_back_end/src/main/java/fr/etic/brp/brp_back_end/metier/modele/SousFamille.java
+++ code/BRP_back_end/src/main/java/fr/etic/brp/brp_back_end/metier/modele/SousFamille.java
@@ -16,7 +16,7 @@ import javax.persistence.OneToMany;
 public class SousFamille implements Serializable {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idSousFamille;
     private String intituleSousFamille;
     
