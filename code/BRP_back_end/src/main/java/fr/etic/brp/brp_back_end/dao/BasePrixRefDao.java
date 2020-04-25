687e74df77b6c2e4558186f3933408b723c14443
lperrious <louis30@me.com>
2020-04-25 10:34:10 +0200

    ajout nvelle DAO

---
+++ code/BRP_back_end/src/main/java/fr/etic/brp/brp_back_end/dao/BasePrixRefDao.java
@@ -0,0 +1,39 @@
+package fr.etic.brp.brp_back_end.dao;
+
+import fr.etic.brp.brp_back_end.metier.modele.BasePrixRef;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author louisrob
+ */
+public class BasePrixRefDao {
+    
+    public void Creer(BasePrixRef basePrixRef){
+        EntityManager em = JpaUtil.obtenirContextePersistance();
+        em.persist(basePrixRef);
+    }
+    
+    public BasePrixRef ChercherParId(Long idBasePrixRef) {
+        EntityManager em = JpaUtil.obtenirContextePersistance();
+        return em.find(BasePrixRef.class, idBasePrixRef); // renvoie null si l'identifiant n'existe pas
+    }
+    
+    public List<BasePrixRef> ListerCategorieConstructions() {
+        EntityManager em = JpaUtil.obtenirContextePersistance();
+        TypedQuery<BasePrixRef> query = em.createQuery("SELECT c FROM CaractDim c", BasePrixRef.class);
+        return query.getResultList();
+    }
+    
+    public void Update(BasePrixRef basePrixRef) {
+        EntityManager em = JpaUtil.obtenirContextePersistance();
+        em.merge(basePrixRef);
+    }
+    
+    public void Remove(BasePrixRef basePrixRef) {
+        EntityManager em = JpaUtil.obtenirContextePersistance();
+        em.remove(basePrixRef);
+    }
+}
---
+++ code/BRP_back_end/src/main/java/fr/etic/brp/brp_back_end/dao/CaractDimDao.java
@@ -0,0 +1,39 @@
+package fr.etic.brp.brp_back_end.dao;
+
+import fr.etic.brp.brp_back_end.metier.modele.CaractDim;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author louisrob
+ */
+public class CaractDimDao {
+    
+    public void Creer(CaractDim caractDim){
+        EntityManager em = JpaUtil.obtenirContextePersistance();
+        em.persist(caractDim);
+    }
+    
+    public CaractDim ChercherParId(Long idCaractDim) {
+        EntityManager em = JpaUtil.obtenirContextePersistance();
+        return em.find(CaractDim.class, idCaractDim); // renvoie null si l'identifiant n'existe pas
+    }
+    
+    public List<CaractDim> ListerCategorieConstructions() {
+        EntityManager em = JpaUtil.obtenirContextePersistance();
+        TypedQuery<CaractDim> query = em.createQuery("SELECT c FROM CaractDim c", CaractDim.class);
+        return query.getResultList();
+    }
+    
+    public void Update(CaractDim caractDim) {
+        EntityManager em = JpaUtil.obtenirContextePersistance();
+        em.merge(caractDim);
+    }
+    
+    public void Remove(CaractDim caractDim) {
+        EntityManager em = JpaUtil.obtenirContextePersistance();
+        em.remove(caractDim);
+    }
+}
