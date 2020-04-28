e456d3d103a73173597d7cce2f4076c2b60503dd
quentinmarc <quentinmarc81@gmail.com>
2020-04-28 11:59:28 +0200

    XML services

---
+++ code/BRP_back_end/src/main/java/fr/etic/brp/brp_back_end/metier/service/ExportService.java
@@ -0,0 +1,13 @@
+package fr.etic.brp.brp_back_end.metier.service;
+
+/**
+ *
+ * @author quentinmarc
+ */
+public class ExportService {
+    
+    //TO DO - Applique les stylesheet XLST au XML du projet puis exporte la CCTP (Word) et la DPGF (Excel)
+    public Boolean ExporterProjet(Long idProjet){
+        return null;
+    }
+}
