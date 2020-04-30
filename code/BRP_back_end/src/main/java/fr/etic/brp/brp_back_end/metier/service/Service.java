523fd381bdb12b8f5583960b152c8f7b19be90b8
lperrious <louis30@me.com>
2020-04-30 11:39:09 +0200

    Suppression BD si erreur XML

--- code/BRP_back_end/src/main/java/fr/etic/brp/brp_back_end/metier/service/Service.java
+++ code/BRP_back_end/src/main/java/fr/etic/brp/brp_back_end/metier/service/Service.java
@@ -281,10 +281,10 @@ public class Service {
         return resultat;
     }
     
-    //TO DO - Supprimer le projet dans la BD si erreur XML !
     public Boolean CreerProjet(String nomProjet) {
         JpaUtil.creerContextePersistance();
         Projet newProjet = null;
+        Long idProjet = null;
         
         try {
             //on crée l'objet projet
@@ -296,7 +296,7 @@ public class Service {
             
             //Creation du XML si tout a fonctionné
             Projet projet = projetDao.ChercherDernierParNom(nomProjet);
-            Long idProjet = projet.getIdProjet();
+            idProjet = projet.getIdProjet();
             String uri = "../XMLfiles/"+idProjet+".xml";
             Document xml = projetXMLDao.Creer();
             
@@ -310,8 +310,15 @@ public class Service {
               
         } catch (Exception ex) {
             Logger.getAnonymousLogger().log(Level.WARNING, "Exception lors de l'appel au Service CreerProjet(nomProjet)", ex);
+            //Supprimer le projet dans la BD si erreur XML
+            try {
+                JpaUtil.ouvrirTransaction();
+                projetDao.Remove(newProjet);
+                JpaUtil.validerTransaction();
+            } catch (Exception e) {
+                Logger.getAnonymousLogger().log(Level.WARNING, "Le projet n°" + idProjet + " n'a pas pu être supprimer ! Il faut le supprimer à la main !", e);
+            }
             newProjet = null;
-            //Supprimer le projet dans la BD !
         } finally {
             JpaUtil.fermerContextePersistance();
         }
@@ -595,11 +602,12 @@ public class Service {
         return null;
     }
     
-    //TO DO - Supprier le projet dupliqué de la BD si erreur XML !
     //Duplique un projet en donnant par défaut le nom "Nouveau Projet"
     public Boolean DupliquerProjet(Long idProjetADupliquer){
         Projet projetADupliquer = null;
         Projet projetDuplique = null;
+        Long idProjet = null;
+        
         JpaUtil.creerContextePersistance();
         
         try {
@@ -626,7 +634,7 @@ public class Service {
                     
                     //Copie du XML si tout a fonctionné
                     Projet projet = projetDao.ChercherDernierParNom(nomProjetDuplique);
-                    Long idProjet = projet.getIdProjet();
+                    idProjet = projet.getIdProjet();
                     String uriNewXML = "../XMLfiles/"+idProjet+".xml";
                     String uriOldXML = "../XMLfiles/"+idProjetADupliquer+".xml";
                     Document xml = projetXMLDao.ObtenirDocument(uriOldXML);
@@ -643,9 +651,16 @@ public class Service {
             }
         } catch (Exception ex) {
             Logger.getAnonymousLogger().log(Level.WARNING, "Exception lors de l'appel au Service DupliquerProjet(idProjet)", ex);
+            //Supprimer le projet dans la BD si erreur XML
+            try {
+                JpaUtil.ouvrirTransaction();
+                projetDao.Remove(projetDuplique);
+                JpaUtil.validerTransaction();
+            } catch (Exception e) {
+                Logger.getAnonymousLogger().log(Level.WARNING, "Le projet n°" + idProjet + " n'a pas pu être supprimer ! Il faut le supprimer à la main !", e);
+            }
             projetADupliquer = null;
             projetDuplique = null;
-            //Supprimer le projet dupliqué de la BD !
         } finally {
             JpaUtil.fermerContextePersistance();
         }
