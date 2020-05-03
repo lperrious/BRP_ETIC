e7f679bbbfebc9a44b03f82e86e494761517b40b
quentinmarc <quentinmarc81@gmail.com>
2020-05-03 11:44:26 +0200

    AjouterLigneChiffrage()

--- code/BRP_back_end/src/main/java/fr/etic/brp/brp_back_end/metier/service/Service.java
+++ code/BRP_back_end/src/main/java/fr/etic/brp/brp_back_end/metier/service/Service.java
@@ -38,6 +38,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 /**
