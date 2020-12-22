541a8189685d7f5291132cc9dcfe4aa3992a8ffd
Louis ROB <louisrob@mbp-de-louis.home>
2020-12-22 22:33:29 +0100

    setup for fixes

--- code/BRP_back_end/target/generated-sources/annotations/fr/etic/brp/brp_back_end/metier/modele/Generique_.java
+++ code/BRP_back_end/target/generated-sources/annotations/fr/etic/brp/brp_back_end/metier/modele/Generique_.java
@@ -3,7 +3,7 @@ package fr.etic.brp.brp_back_end.metier.modele;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-21T21:54:50")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-12-22T22:30:54")
 @StaticMetamodel(Generique.class)
 public class Generique_ extends Descriptif_ {
 
