b1f9ac6572838a8bb1620ff6714ff2dd9fec4910
quentinmarc <quentinmarc81@example.com>
2021-01-03 19:06:04 +0100

    install Quentin

--- code/BRP_back_end/target/generated-sources/annotations/fr/etic/brp/brp_back_end/metier/modele/Prestation_.java
+++ code/BRP_back_end/target/generated-sources/annotations/fr/etic/brp/brp_back_end/metier/modele/Prestation_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-12-23T10:43:44")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-01-03T18:26:58")
 @StaticMetamodel(Prestation.class)
 public class Prestation_ extends Descriptif_ {
 
